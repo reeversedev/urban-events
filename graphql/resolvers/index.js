08c2826eb6e619a66f83f6406e10c91cf11ec753
Prateek Gogia <gogia.prateek@hotmail.com>
2019-01-23 15:41:12 +0530

    Refactored the date and booking

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,14 +2,27 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
 
+const { dateToString } = require('../../helpers/date');
+
 const bcrypt = require('bcryptjs');
 
 const transformEvent = event => {
   return {
     ...event._doc,
     _id: event.id,
-    date: new Date(event._doc.date).toISOString(),
+    date: dateToString(event._doc.date),
     creator: user.bind(this, event.creator)
+  };
+};
+
+const transformBooking = booking => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt)
   };
 };
 
@@ -62,14 +75,7 @@ module.exports = {
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
-        return {
-          ...booking._doc,
-          _id: booking.id,
-          user: user.bind(this, booking._doc.user),
-          event: singleEvent.bind(this, booking._doc.event),
-          createdAt: new Date(booking._doc.createdAt).toISOString(),
-          updatedAt: new Date(booking._doc.updatedAt).toISOString()
-        };
+        return transformBooking(booking);
       });
     } catch (err) {
       throw err;
@@ -123,14 +129,7 @@ module.exports = {
         event: fetchedEvent
       });
       const result = await booking.save();
-      return {
-        ...result._doc,
-        _id: result.id,
-        user: user.bind(this, booking._doc.user),
-        event: singleEvent.bind(this, booking._doc.event),
-        createdAt: new Date(result._doc.createdAt).toISOString(),
-        updatedAt: new Date(result._doc.updatedAt).toISOString()
-      };
+      return transformBooking(result);
     } catch (err) {
       throw err;
     }
